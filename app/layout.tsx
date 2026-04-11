e3acddf73db6cfe01a470692e280c4d5f8c8e287
madebyfelipe <byonichip@gmail.com>
2026-04-11 19:45:55 -0300

    <!-- Meta Pixel Code --> <script> !function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1465893921656330'); fbq('track', 'PageView'); </script> <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1465893921656330&ev=PageView&noscript=1" /></noscript> <!-- End Meta Pixel Code -->

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1306925334660468');
+fbq('init', '1465893921656330');
 fbq('track', 'PageView');`}
         </Script>
         <noscript>
@@ -52,7 +52,7 @@ fbq('track', 'PageView');`}
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1306925334660468&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1465893921656330&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
